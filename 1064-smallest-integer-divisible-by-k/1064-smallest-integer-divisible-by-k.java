8f8252912a083576284539f8756422c318c676b4
Shashank Mishra <bsmshashankmishra@gmail.com>
2025-03-27 12:28:56 +0530

    Time: 3 ms (28.07%), Space: 40.5 MB (56.14%) - LeetHub

---
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        long n=0;
+        if(k%2==0){
+            return -1;
+        }
+        else{
+            for(int i =1;i<=k; i++){
+                n=(n*10+1)%k;//-->here n get reduced earlier so it will not effect in if // n=n*10+1;(not this)
+                if(n==0){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
