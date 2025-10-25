236c4167be7c2173931c6ae5b7c9adbab4f7b5c6
Shashank Mishra <bsmshashankmishra@gmail.com>
2025-10-25 10:37:46 +0530

    Time: 4 ms (59.88%), Space: 42 MB (95.67%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         int counter=0;
-        String ans="";
+        StringBuilder ans= new StringBuilder();
         for(int i =0; i<s.length();i++){
             if(s.charAt(i)=='('){
                 if(counter!=0){
-                    ans +='(';
+                    ans.append('(');
                 }
                 counter +=1;
             }
             else{
                 counter -= 1;
                 if(counter !=0){
-                    ans +=')';
+                    ans.append(')');
                 }
                 
             }
         }
-        return ans;
+        return ans.toString();
 
     }
 }
