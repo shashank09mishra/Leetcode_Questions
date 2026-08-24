dc20dbe2d2363dd050865a2812977678321ff6fd
Shashank Mishra <bsmshashankmishra@gmail.com>
2026-08-24 18:47:07 +0530

    Time: 15 ms (23.95%), Space: 46.6 MB (16.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
         HashMap<Character,Integer> map1= new HashMap<>();
         HashMap<Character,Integer> map2= new HashMap<>();
 
